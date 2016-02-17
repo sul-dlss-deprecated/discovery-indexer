72fef029d6c655e8e6520db0c600fad280974ea5
Naomi Dushay <ndushay@stanford.edu>
2016-02-17 12:28:22 -0800

    update comments for purlxml class

--- lib/discovery-indexer/reader/purlxml.rb
+++ lib/discovery-indexer/reader/purlxml.rb
@@ -1,25 +1,27 @@
 module DiscoveryIndexer
   module InputXml
-    # This class is the main class to access and parse the purl xml
-    #    as retrieved from PURL server
+    # Main model class to access the parsed purl xml retrieved from PURL server
     # @example to run the code
-    #  druid = "aa111aa1111"
-    #  p =  DiscoveryIndexer::InputXml::Purlxml.new(druid)
-    #  model =  p.load()
+    #    druid = "aa111aa1111"
+    #    p =  DiscoveryIndexer::InputXml::Purlxml.new(druid)
+    #    model =  p.load()
+    #  then you can access the bits of interest
+    #    model.collection_druids
     class Purlxml
       # initializes a new object
       # @param druid [String] the druid object in the format "aa111aa1111"
       def initialize(druid)
         @druid = druid
         @purlxml_ng_doc = nil
+        @populated_model = nil
       end
 
       # loads the purl xml to purlxml model for the fedora object defind in the druid,
       # it reads the purl xml once from PURL server, and repeat the parsing with each call
       # @return [PurlxmlModel] represents the purlxml
       def load
-        @purlxml_ng_doc = PurlxmlReader.read(@druid) if @purlxml_ng_doc.nil?
-        PurlxmlParserStrict.new(@druid, @purlxml_ng_doc).parse
+        @purlxml_ng_doc ||= PurlxmlReader.read(@druid)
+        @populated_model ||= PurlxmlParserStrict.new(@druid, @purlxml_ng_doc).parse
       end
     end
   end
